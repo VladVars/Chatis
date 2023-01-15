b0014b96cff4b64faad977341a82a6b0e2973d79
VladVars <105005032+VladVars@users.noreply.github.com>
2023-01-15 15:12:02 +0300

    started

---
+++ Chatis/Chatis/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatis
+//
+//  Created by VladVarsotski on 15.01.23.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
